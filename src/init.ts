e833267793e5f44b054896e985e6907d01407727
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-07-30 14:02:13 +0530

    added pan in and pan out

--- src/init.ts
+++ src/init.ts
@@ -4,6 +4,10 @@ import main from "./main";
 /** creating a fresh canvas only if webgl2 is available, otherwise error message is shown. */
 if (WebGL.isWebGL2Available()) {
   main();
-} else {
+} else if (!WebGL.isWebGL2Available()) {
   document.body.appendChild(WebGL.getWebGL2ErrorMessage());
+} else {
+  const error = document.createElement("div");
+  error.textContent = "Page not Found";
+  document.body.appendChild(error);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,53 @@
 import * as THREE from "three";
+import Stats from "stats.js";
+
+var stats = new Stats();
+stats.showPanel(0);
+document.body.appendChild(stats.dom);
 
 /** Start of Solace CAD */
 function main() {
   const canvas = Canvas();
+
   const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x171717);
+
   const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+
   const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 500);
-  const light = new THREE.DirectionalLight(0xffffff, 3);
+  let factor = 2;
+  const cameraDirectionVector = new THREE.Vector3(1, 1, 1).normalize();
+  camera.position.set(
+    factor * cameraDirectionVector.x,
+    factor * cameraDirectionVector.y,
+    factor * cameraDirectionVector.z,
+  );
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-  light.position.set(2, 2, 4);
-  camera.position.set(4, 2, 4);
-  camera.rotateY(Math.PI / 4);
+  window.onwheel = (event): void => {
+    factor += event.deltaY;
+    camera.position.set(
+      factor * cameraDirectionVector.x,
+      factor * cameraDirectionVector.y,
+      factor * cameraDirectionVector.z,
+    );
+    camera.updateMatrix();
+  };
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
-  const cube = new THREE.Mesh(geometry, material);
-  const gridHelper = new THREE.GridHelper(10, 10);
-
-  scene.add(cube);
-  scene.add(light);
+  const gridHelper = new THREE.GridHelper(20, 20);
   scene.add(gridHelper);
 
-  renderer.setAnimationLoop(() => animate(cube, renderer, scene, camera));
+  renderer.setAnimationLoop(animate);
+  function animate() {
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    renderer.render(scene, camera);
+    stats.update();
+  }
 }
 
 /** @return {HTMLCanvasElement} An empty canvas with id="c". */
@@ -32,29 +58,7 @@ function Canvas(): HTMLCanvasElement {
   return canvas;
 }
 
-function animate(
-  cube: THREE.Mesh<
-    THREE.BoxGeometry,
-    THREE.MeshPhongMaterial,
-    THREE.Object3DEventMap
-  >,
-  renderer: THREE.WebGLRenderer,
-  scene: THREE.Scene,
-  camera: THREE.PerspectiveCamera,
-) {
-  if (resizeRendererToDisplaySize(renderer)) {
-    const canvas = renderer.domElement;
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
-  }
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
-}
-
-function resizeRendererToDisplaySize(renderer: THREE.WebGLRenderer) {
+function resizeRendererToDisplaySize(renderer: THREE.WebGLRenderer): boolean {
   const canvas = renderer.domElement;
   const pixelRatio = window.devicePixelRatio;
   const width = Math.floor(canvas.clientWidth * pixelRatio);
